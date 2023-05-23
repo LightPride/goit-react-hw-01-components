050cd899247a116c4c51aba7b6af9c5d5ae67a2e
LightPride <vlad.grinevich1@gmail.com>
2023-05-23 20:01:12 +0300

    fix

--- src/components/FriendListItem/FriendListitem.jsx
+++ src/components/FriendListItem/FriendListitem.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { FriendCard, Status, Avatar, Name } from './FriendListItem.styled';
-export const FriendListItem = ({ avatar, name, isOnline, id }) => {
+export const FriendListItem = ({ avatar, name, isOnline }) => {
   return (
     <FriendCard>
       <Status isOnline={isOnline}>{isOnline}</Status>
@@ -8,4 +9,10 @@ export const FriendListItem = ({ avatar, name, isOnline, id }) => {
       <Name>{name}</Name>
     </FriendCard>
   );
+};
+
+FriendListItem.propTypes = {
+  avatar: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  isOnline: PropTypes.bool.isRequired,
 };
